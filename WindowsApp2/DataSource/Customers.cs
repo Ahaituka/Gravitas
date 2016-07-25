10d4c1bd548e4db98253aeff003237bf7708ac0d
bitloks <bitloks@gmail.com>
2016-07-25 15:04:37 +0530

    Added Search Functionality

---
+++ WindowsApp2/DataSource/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp2.DataSource
+{
+   public class Customers
+    {
+        public string name;
+        public override string ToString()
+        {
+            return string.Format("{0}", name);
+        }
+    }
+}
--- WindowsApp2/Views/MainPage.xaml.cs
+++ WindowsApp2/Views/MainPage.xaml.cs
@@ -4,16 +4,23 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using System.Collections.ObjectModel;
+using WindowsApp2.DataSource;
+using System.Linq;
 
 namespace WindowsApp2.Views
 {
     public sealed partial class MainPage : Page
     {
+
+        SearchQueries searchQueries = new SearchQueries();
+
         public MainPage()
         {
             InitializeComponent();
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
         }
+
+
 
         /// <summary>
         /// This event gets fired anytime the text in the TextBox gets updated.
@@ -32,7 +39,13 @@ namespace WindowsApp2.Views
 
                     sender.ItemsSource = matchingContacts.ToList();
                 }
+
     */
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                var matchingCustomer = searchQueries.GetMatchingCustomer(sender.Text);
+                sender.ItemsSource = matchingCustomer.ToList();
+            }
         }
 
 
@@ -47,56 +60,78 @@ namespace WindowsApp2.Views
         /// and also ChosenSuggestion, which is only non-null when a user selects an item in the list.</param>
         private void asb_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
+
+
             if (args.ChosenSuggestion != null)
             {
-                //User selected an item, take an action on it here
-                //    SelectContact(args.ChosenSuggestion as Contact);
+                SelectCustomer(args.ChosenSuggestion as Customers);
             }
             else
             {
-                //Do a fuzzy search on the query text
-                /*  var matchingContacts = ContactSampleDataSource.GetMatchingContacts(args.QueryText);
-
-                  if (matchingContacts.Count() >= 1)
-                  {
-                      //Choose the first match
-                      SelectContact(matchingContacts.FirstOrDefault());
-                  }
-                  else
-                  {
-                      NoResults.Visibility = Visibility.Visible;
-                  }*/
-            }
-        }
-
-        /// <summary>
-        /// This event gets fired as the user keys through the list, or taps on a suggestion.
-        /// This allows you to change the text in the TextBox to reflect the item in the list.
-        /// Alternatively you can use TextMemberPath.
-        /// </summary>
-        /// <param name="sender">The AutoSuggestBox that fired the event.</param>
-        /// <param name="args">The args contain SelectedItem, which contains the data item of the item that is currently highlighted.</param>
-        private void asb_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
-        {
-            /*   var contact = args.SelectedItem as Contact;
-
-               sender.Text = string.Format("{0} ({1})", contact.FullName, contact.Company);*/
-        }
-
-        /// <summary>
-        /// This 
-        /// </summary>
-        /// <param name="contact"></param>
-        /*private void SelectContact(Contact contact)
-        {
-            if (contact != null)
-            {
-                ContactDetails.Visibility = Visibility.Visible;
-                ContactName.Text = contact.FullName;
-                ContactCompany.Text = contact.Company;
+                var matchingCustomer = searchQueries.GetMatchingCustomer(args.QueryText);
+                if (matchingCustomer.Count() >= 1)
+                    SelectCustomer(matchingCustomer.FirstOrDefault());
             }
 
-        }*/
+            /* if (args.ChosenSuggestion != null)
+             {
+                 //User selected an item, take an action on it here
+                 //    SelectContact(args.ChosenSuggestion as Contact);
+             }
+             else
+             {
+                 //Do a fuzzy search on the query text
+                 /*  var matchingContacts = ContactSampleDataSource.GetMatchingContacts(args.QueryText);
+
+                   if (matchingContacts.Count() >= 1)
+                   {
+                       //Choose the first match
+                       SelectContact(matchingContacts.FirstOrDefault());
+                   }
+                   else
+                   {
+                       NoResults.Visibility = Visibility.Visible;
+                   }*/
+        }
+
+        private void SelectCustomer(Customers customers)
+        {
+            if (customers != null)
+                asb.Text = customers.name;
+        }
+    
+
+    /// <summary>
+    /// This event gets fired as the user keys through the list, or taps on a suggestion.
+    /// This allows you to change the text in the TextBox to reflect the item in the list.
+    /// Alternatively you can use TextMemberPath.
+    /// </summary>
+    /// <param name="sender">The AutoSuggestBox that fired the event.</param>
+    /// <param name="args">The args contain SelectedItem, which contains the data item of the item that is currently highlighted.</param>
+    private void asb_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
+        /*   var contact = args.SelectedItem as Contact;
+
+           sender.Text = string.Format("{0} ({1})", contact.FullName, contact.Company);*/
     }
 
+
+
+    /// <summary>
+    /// This 
+    /// </summary>
+    /// <param name="contact"></param>
+    /*private void SelectContact(Contact contact)
+    {
+        if (contact != null)
+        {
+            ContactDetails.Visibility = Visibility.Visible;
+            ContactName.Text = contact.FullName;
+            ContactCompany.Text = contact.Company;
+        }
+
+    }*/
 }
+    }
+
+
