53c2f1d820d5cee3d64187b329879c0efd919afc
bitloks <bitloks@gmail.com>
2016-08-31 16:01:24 +0530

    detail backstack corrected

--- WindowsApp2/Views/DetailPage.xaml.cs
+++ WindowsApp2/Views/DetailPage.xaml.cs
@@ -42,17 +42,23 @@ namespace WindowsApp2.Views
         public Event Item
         {
             get { return (Event)GetValue(s_itemProperty); }
-            set { SetValue(s_itemProperty, value); }  
+            set { SetValue(s_itemProperty, value); }
         }
-        
+
         public DetailPage()
         {
             this.InitializeComponent();
         }
 
-        protected async  override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-            base.OnNavigatedTo(e);
+
+            var param = e.Parameter?.ToString();
+            var service = Template10.Services.SerializationService.SerializationService.Json;
+            var value = service.Deserialize<string>(param);
+
+
+
 
             //ObservableCollection<Event> eventDataWrapper =  EventList.Select((x) => x.name = (string)e.Parameter)
 
@@ -62,10 +68,10 @@ namespace WindowsApp2.Views
             //{
             //    Item.Add(character);
 
-            
+
             //}
 
-           
+
 
             /*
             this.EventList = DataManager.EventList;
@@ -79,12 +85,12 @@ namespace WindowsApp2.Views
             //var observable = new ObservableCollection<Foo>(linqResults);
 
             // Item = DataManager.EventList.First((ev) => ev.name == (string)e.Parameter);
-            //Item = DataManager.EventList.Where((ev) => ev.name == (string)e.Parameter).FirstOrDefault();
+            Item = DataManager.EventList.Where((ev) => ev.name == value).FirstOrDefault();
 
             //List<Event> Source = Item.ToList<Event>();
             //Source
 
-           
+
             //var backStack = Frame.BackStack;
             //var backStackCount = backStack.Count;
 
@@ -102,90 +108,92 @@ namespace WindowsApp2.Views
             //        );
             //    backStack.Add(modifiedEntry);
             //}
-            
-            //// Register for hardware and software back request from the system
-            //SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
-            //systemNavigationManager.BackRequested += DetailPage_BackRequested;
-            //systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+            // Register for hardware and software back request from the system
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+       //   systemNavigationManager.BackRequested += DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+            base.OnNavigatedTo(e);
         }
 
-        //protected override void OnNavigatedFrom(NavigationEventArgs e)
-        //{
-        //    base.OnNavigatedFrom(e);
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
 
-        //    SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
-        //    systemNavigationManager.BackRequested -= DetailPage_BackRequested;
-        //    systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
-        //}
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+       //     systemNavigationManager.BackRequested -= DetailPage_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
 
         //private void OnBackRequested()
         //{
         //    // Page above us will be our master view.
         //    // Make sure we are using the "drill out" animation in this transition.
-            
+
         //    Frame.GoBack(new DrillInNavigationTransitionInfo());
         //}
 
-        //void NavigateBackForWideState(bool useTransition)
+        void NavigateBackForWideState(bool useTransition)
+        {
+            // Evict this page from the cache as we may not need it again.
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+
+            if (useTransition)
+            {
+                Frame.GoBack(new EntranceNavigationTransitionInfo());
+            }
+            else
+            {
+                Frame.GoBack(new SuppressNavigationTransitionInfo());
+            }
+        }
+
+        private bool ShouldGoToWideState()
+        {
+            return Window.Current.Bounds.Width >= 720;
+        }
+
+        private void PageRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ShouldGoToWideState())
+            {
+                // We shouldn't see this page since we are in "wide master-detail" mode.
+                // Play a transition as we are navigating from a separate page.
+                NavigateBackForWideState(useTransition: true);
+            }
+            else
+            {
+                // Realize the main page content.
+                FindName("RootPanel");
+            }
+
+            Window.Current.SizeChanged += Window_SizeChanged;
+        }
+
+        private void PageRoot_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Window.Current.SizeChanged -= Window_SizeChanged;
+        }
+
+        private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            if (ShouldGoToWideState())
+            {
+                // Make sure we are no longer listening to window change events.
+                Window.Current.SizeChanged -= Window_SizeChanged;
+
+                // We shouldn't see this page since we are in "wide master-detail" mode.
+                NavigateBackForWideState(useTransition: false);
+            }
+        }
+
+        //private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
         //{
-        //    // Evict this page from the cache as we may not need it again.
-        //    NavigationCacheMode = NavigationCacheMode.Disabled;
+        //    // Mark event as handled so we don't get bounced out of the app.
+        //    e.Handled = true;
 
-        //    if (useTransition)
-        //    {
-        //        Frame.GoBack(new EntranceNavigationTransitionInfo());
-        //    }
-        //    else
-        //    {
-        //        Frame.GoBack(new SuppressNavigationTransitionInfo());
-        //    }
+        //    OnBackRequested();
         //}
-
-    //    private bool ShouldGoToWideState()
-    //    {
-    //        return Window.Current.Bounds.Width >= 720;
-    //    }
-
-    //    private void PageRoot_Loaded(object sender, RoutedEventArgs e)
-    //    {
-    //        if (ShouldGoToWideState())
-    //        {
-    //            // We shouldn't see this page since we are in "wide master-detail" mode.
-    //            // Play a transition as we are navigating from a separate page.
-    //            NavigateBackForWideState(useTransition:true);
-    //        }
-    //        else
-    //        {
-    //            // Realize the main page content.
-    //            FindName("RootPanel");
-    //        }
-
-    //        Window.Current.SizeChanged += Window_SizeChanged;
-    //    }
-
-    //    private void PageRoot_Unloaded(object sender, RoutedEventArgs e)
-    //    {
-    //        Window.Current.SizeChanged -= Window_SizeChanged;
-    //    }
-
-    //    private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
-    //    {
-    //        if (ShouldGoToWideState())
-    //        {
-    //            // Make sure we are no longer listening to window change events.
-    //            Window.Current.SizeChanged -= Window_SizeChanged;
-
-    //            // We shouldn't see this page since we are in "wide master-detail" mode.
-    //            NavigateBackForWideState(useTransition:false);
-    //        }
-    //    }
-
-    //    //private void DetailPage_BackRequested(object sender, BackRequestedEventArgs e)
-    //    //{
-    //    //    // Mark event as handled so we don't get bounced out of the app.
-    //    //    e.Handled = true;
-
-    //    //    OnBackRequested();
-    //    //}
     }
 }
